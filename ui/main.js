16c4e538b127486d46533dc8ce28b4d11ba5513b
adityakodte <2015bcs073@sggs.ac.in>
2017-03-16 23:38:41 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -7,6 +7,6 @@ element.innerHTML = "New value";
 
 //move the image
 var image = document.getElementById('madi');
-img.onclick = function() {
+madi.onclick = function() {
   img.style.marginLeft = '100px';
 };
