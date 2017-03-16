baadfa572637d83e3e78029d1d1aac9e703ff25c
adityakodte <2015bcs073@sggs.ac.in>
2017-03-16 23:13:15 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,5 +2,5 @@ console.log('Loaded!');
 
 //change the text of the main text div
 
-var element = document.getElementbyId('main-text');
+var element = document.getElementById('main-text');
 element.innerHTML = "new value";
