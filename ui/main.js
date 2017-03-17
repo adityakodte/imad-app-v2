a1fad53e34cc9f6ffea1dda6d0983f22a38946f3
adityakodte <2015bcs073@sggs.ac.in>
2017-03-17 13:01:50 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -3,52 +3,51 @@ var button = document.getElementById('counter');
 
 button.onclick = function () {
   
-  //create a request object
-  var request = new XMLHttpRequest();
+    //create a request object
+    var request = new XMLHttpRequest();
   
-  //capture the response and store it in a variable
-  request.onreadystatechange = function() {
-      if(request.readyState === XMLHttpRequest.DONE) {
-          //Take some action
-          if (request.status === 200) {
-              var counter = request.responseText
+    //capture the response and store it in a variable
+    request.onreadystatechange = function() {
+        if(request.readyState === XMLHttpRequest.DONE) {
+            //Take some action
+            if (request.status === 200) {
+               var counter = request.responseText
                var span = document.getElementById('count');
                span.innerHTML = counter.toString();
-          }
-      }
-      //Not done yet
-  };
- 
-  //Make the request
-  request.open('GET','http://adityakodte.imad.hasura-app.io/counter', true);
-  request.send(null);
-};
-
-//Submit name
-var submit = document.getElementById('submit_btn');
-submit.onclick = function () {
-    
-    //create a request object
-  var request = new XMLHttpRequest();
-  
-  //capture the response and store it in a variable
-  request.onreadystatechange = function() {
-      if(request.readyState === XMLHttpRequest.DONE) {
-          //Take some action
-          if (request.status === 200) {
-                  //Capture a list of names and render it as a list.
-    var names = request.responseText;
-    names = JSON.parse(names);
-    var list = '';
-    for (var i=0; i<names.length; i++) {
-        list += '<li>' + names[i] + '</li>';
+            }
         }
-        var ul = document.getElementById('namelist');
-        ul.innerHTML = list;
-              
-          }
-      }
-      //Not done yet
+        //Not done yet
+    };
+ 
+    //Make the request
+    request.open('GET','http://adityakodte.imad.hasura-app.io/counter', true);
+    request.send(null);
+    };
+
+    //Submit name
+    var submit = document.getElementById('submit_btn');
+    submit.onclick = function () {
+    
+        //create a request object
+        var request = new XMLHttpRequest();
+  
+        //capture the response and store it in a variable
+        request.onreadystatechange = function() {
+                if(request.readyState === XMLHttpRequest.DONE) {
+                //Take some action
+                if (request.status === 200) {
+                    //Capture a list of names and render it as a list.
+                    var names = request.responseText;
+                    names = JSON.parse(names);
+                    var list = '';
+                        for (var i=0; i<names.length; i++) {
+                        list += '<li>' + names[i] + '</li>';
+                    }
+                    var ul = document.getElementById('namelist');
+                    ul.innerHTML = list;
+                    }
+        }
+         //Not done yet
   };
  
   //Make the request
