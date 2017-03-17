3c0a80ae8fd4455d69100e85a529757fd452f11b
adityakodte <2015bcs073@sggs.ac.in>
2017-03-17 12:26:49 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -100,10 +100,22 @@ app.get('/:articleName', function(req, res){
    res.send(createTemplate(articles[articleName]));
 });
 
+var names = [];
 app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
 
+app.get('/submit-name/:name', function(req,res) {
+    //Get the name from the request
+    
+    var name = req.params.name;
+    
+    
+    names.push(name);
+    //JSON: Javascript Object Notation
+    res.send(JSON.stringify(names));
+    res.send(names);//TODO
+});
 
 var port = 8080; // Use 8080 for local development because you might already have apache running on 80
 app.listen(8080, function () {
